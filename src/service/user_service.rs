8044081b5b45594358c0728654697e43e52c2d9c
Antonio Frisenda <antoniofrisenda.dev@gmail.com>
2024-09-15 11:38:13 +0200

    Removed unused import

--- src/service/user_service.rs
+++ src/service/user_service.rs
@@ -7,7 +7,6 @@ use async_trait::async_trait;
 use axum::extract::FromRef;
 use mongodb::bson::oid::ObjectId;
 use std::sync::Arc;
-use axum::Json;
 use validator::Validate;
 use crate::support::sqs_listener::SqsListener;
 
